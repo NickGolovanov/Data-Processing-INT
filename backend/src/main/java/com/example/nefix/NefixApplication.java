728bb59c920ddfb605ed564db14f9aff01640a10
0Empty0 <garg.victor11@gmail.com>
2024-11-26 11:12:42 +0100

    docker setup

---
+++ backend/src/main/java/com/example/nefix/NefixApplication.java
@@ -0,0 +1,13 @@
+package com.example.nefix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NefixApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NefixApplication.class, args);
+    }
+
+}
