92b1409e436bdc6c2777a40fb273ffa129913208
0Empty0 <garg.victor11@gmail.com>
2024-12-09 15:16:01 +0100

    update blocked accounts url

--- backend/src/main/java/com/example/nefix/blockedaccount/BlockedAccountController.java
+++ backend/src/main/java/com/example/nefix/blockedaccount/BlockedAccountController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/blockedaccount")
+@RequestMapping("/blocked-account")
 public class BlockedAccountController extends BaseController<BlockedAccount, Long>
 {
     public BlockedAccountController(BlockedAccountService service)
