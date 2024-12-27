dbcf5c7663d3b8bb07c3f9e40e99cfa26a97ebf0
0Empty0 <garg.victor11@gmail.com>
2024-12-27 22:56:42 +0100

    enable scheduling

--- backend/src/main/java/com/example/nefix/NefixApplication.java
+++ backend/src/main/java/com/example/nefix/NefixApplication.java
@@ -2,12 +2,14 @@ package com.example.nefix;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-public class NefixApplication {
-
-    public static void main(String[] args) {
+@EnableScheduling
+public class NefixApplication
+{
+    public static void main(String[] args)
+    {
         SpringApplication.run(NefixApplication.class, args);
     }
-
 }
