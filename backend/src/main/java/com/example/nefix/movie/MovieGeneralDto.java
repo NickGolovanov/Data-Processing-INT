e82632ff808bddc9cbb24c840ea717aba57ffd6c
TimofeiArefev <timofei.arefev@student.nhlstenden.com>
2025-01-11 22:56:37 +0100

    add website

---
+++ backend/src/main/java/com/example/nefix/movie/MovieGeneralDto.java
@@ -0,0 +1,20 @@
+package com.example.nefix.movie;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Data
+@Getter
+@Setter
+@AllArgsConstructor
+public class MovieGeneralDto {
+        private Long id;
+        private String title;
+
+        public MovieGeneralDto(Movie movie) {
+                this.id = movie.getMovieId();
+                this.title = movie.getTitle();
+        }
+}
